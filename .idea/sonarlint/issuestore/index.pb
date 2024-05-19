9d702b3333f050620f6ad6383dc3090327034551
Elena Gorlanova <e.gorlanova@astondevs.ru>
2024-05-19 10:46:12 +0700

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+I
+BankingOperations/pom.xml,d\8\d8ecc91054f5a2c18e81e6ff1f3880c4a6126a36
+L
+BankingOperations/.gitignore,c\f\cf3a3f86d46778f2411249c33cdb49705d1836ca
---
+++ BankingOperations/src/main/java/com/example/BankingOperations/BankingOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankingOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingOperationsApplication.class, args);
+	}
+
+}
